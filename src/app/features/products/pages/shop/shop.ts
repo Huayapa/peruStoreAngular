1ea3f1771197fe416efcef46dbe443264616bd9c
josue Huayapa <huayapajosue0@gmail.com>
2026-05-01 19:32:28 -0500

    enhance shop page with product filters, dynamic product listing, and breadcrumb integration

--- src/app/features/products/pages/shop/shop.ts
+++ src/app/features/products/pages/shop/shop.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Breadcrumb } from '../../../../shared/components/breadcrumb/breadcrumb';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { ProductService } from '../../../../core/services/product';
+import { CardProduct } from '../../../../shared/components/card-product/card-product';
 
 @Component({
   selector: 'app-shop',
-  imports: [Breadcrumb],
+  imports: [Breadcrumb, CardProduct],
   templateUrl: './shop.html',
   styleUrl: './shop.scss',
 })
-export default class ShopPage {}
+export default class ShopPage {
+  private readonly _product = inject(ProductService);
+  products = toSignal(this._product.getProducts(), { initialValue: [] });
+}

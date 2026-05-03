14c533f935e8883e547434f33451334df2875fb8
josue Huayapa <huayapajosue0@gmail.com>
2026-05-02 20:22:40 -0500

    integration filter products with queryparams

--- src/app/core/services/product.ts
+++ src/app/core/services/product.ts
@@ -3,7 +3,12 @@ import { inject, Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { map, Observable } from 'rxjs';
 import { IProduct } from '../../shared/interfaces/product.interface';
-
+export interface IFilterProduct {
+  categories: string[];
+  search: string;
+  min: number;
+  max: number;
+}
 @Injectable({
   providedIn: 'root',
 })
@@ -13,6 +18,22 @@ export class ProductService {
   getProducts(): Observable<IProduct[]> {
     return this.http.get<IProduct[]>(`${this.urlDomain}/products`);
   }
+
+  getFilteredProducts(filters: IFilterProduct): Observable<IProduct[]> {
+    const { categories, search, min, max } = filters;
+    const normalizedSearch = search.trim().toLowerCase();
+    return this.http.get<IProduct[]>(`${this.urlDomain}/products`).pipe(
+      map((product) =>
+        product.filter((p) => {
+          const matchCategory = categories.length === 0 || categories.includes(p.category);
+          const matchSearch = !normalizedSearch || p.title.toLowerCase().includes(normalizedSearch);
+          const matchPrice = p.price >= (min ?? 0) && p.price <= (max ?? Infinity);
+          return matchCategory && matchSearch && matchPrice;
+        }),
+      ),
+    );
+  }
+
   getCategory(): Observable<string[]> {
     return this.http
       .get<IProduct[]>(`${this.urlDomain}/products`)
--- src/app/features/products/pages/shop/shop.ts
+++ src/app/features/products/pages/shop/shop.ts
@@ -1,16 +1,87 @@
 import { Component, inject } from '@angular/core';
-import { toSignal } from '@angular/core/rxjs-interop';
-import { ProductService } from '../../../../core/services/product';
+import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
+import { IFilterProduct, ProductService } from '../../../../core/services/product';
 import { CardProduct } from '../../../../shared/components/card-product/card-product';
 import { BannerSection } from '../../../home/components/banner-section/banner-section';
+import { NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { MatCheckbox } from '@angular/material/checkbox';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MatIcon } from '@angular/material/icon';
+import { MatSliderModule } from '@angular/material/slider';
+import { BehaviorSubject, debounceTime, switchMap } from 'rxjs';
+import { IProduct } from '../../../../shared/interfaces/product.interface';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-shop',
-  imports: [CardProduct, BannerSection],
+  imports: [
+    CardProduct,
+    BannerSection,
+    ReactiveFormsModule,
+    MatCheckbox,
+    MatIcon,
+    MatSliderModule,
+    AsyncPipe,
+  ],
   templateUrl: './shop.html',
   styleUrl: './shop.scss',
 })
 export default class ShopPage {
+  private readonly _routeActive = inject(ActivatedRoute);
+  private readonly _router = inject(Router);
+  private readonly _form = inject(NonNullableFormBuilder);
   private readonly _product = inject(ProductService);
-  products = toSignal(this._product.getProducts(), { initialValue: [] });
+  private readonly products = new BehaviorSubject<IProduct[]>([]);
+  readonly products$ = this.products.asObservable();
+  categories = toSignal(this._product.getCategory(), { initialValue: [] });
+
+  form = this._form.group({
+    fsearch: '',
+    fcategory: this._form.array<string>([]),
+    fmin: 0,
+    fmax: 10000,
+  });
+
+  constructor() {
+    this._routeActive.queryParams
+      .pipe(
+        takeUntilDestroyed(),
+        switchMap((params) => {
+          const filters: IFilterProduct = {
+            categories: params['category'] ? ([] as string[]).concat(params['category']) : [],
+            search: params['search'] ? String(params['search']) : '',
+            min: params['min'] ? Number(params['min']) : 0,
+            max: params['max'] ? Number(params['max']) : 10000,
+          };
+          return this._product.getFilteredProducts(filters);
+        }),
+      )
+      .subscribe((data) => {
+        this.products.next(data);
+      });
+
+    this.form.valueChanges
+      .pipe(debounceTime(500), takeUntilDestroyed())
+      .subscribe(({ fcategory, fsearch, fmin, fmax }) => {
+        this._router.navigate([], {
+          queryParams: {
+            category: fcategory,
+            search: fsearch,
+            min: fmin,
+            max: fmax,
+          },
+          queryParamsHandling: 'merge',
+        });
+      });
+  }
+
+  onCategoryChange(category: string, checked: boolean) {
+    const fcategory = this.form.controls.fcategory;
+    if (checked) {
+      fcategory.push(this._form.control(category));
+    } else {
+      const index = fcategory.controls.findIndex((c) => c.value === category);
+      fcategory.removeAt(index);
+    }
+  }
 }

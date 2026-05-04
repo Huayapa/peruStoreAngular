92d10d1b0b81d6ca732ec3ad28573e471fa89c3f
josue Huayapa <huayapajosue0@gmail.com>
2026-05-03 20:03:44 -0500

    refactor categories component: add routing to category cards

--- src/app/features/home/components/categories/categories.ts
+++ src/app/features/home/components/categories/categories.ts
@@ -1,14 +1,17 @@
 import { Component, inject } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { ProductService } from '../../../../core/services/product';
+import { RouterLink } from '@angular/router';
+import { APP_ROUTES } from '../../../../core/constants/app-routes';
 
 @Component({
   selector: 'app-categories',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './categories.html',
   styleUrl: './categories.scss',
 })
 export class Categories {
   private readonly _product = inject(ProductService);
+  readonly APP_ROUTES = APP_ROUTES;
   categories = toSignal(this._product.getCategory(), { initialValue: [] });
 }

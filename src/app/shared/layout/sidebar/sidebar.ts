85fbbf6f95315ec060d2c9aa02e368d633e5be6b
josue Huayapa <huayapajosue0@gmail.com>
2026-05-06 20:08:52 -0500

    feat: integrate search bar and cart button into sidebar layout

--- src/app/shared/layout/sidebar/sidebar.ts
+++ src/app/shared/layout/sidebar/sidebar.ts
@@ -4,10 +4,11 @@ import { RouterLink } from '@angular/router';
 import { SidenavService } from '../../../core/services/sidenav';
 import { MatIcon } from '@angular/material/icon';
 import { MatIconButton } from '@angular/material/button';
+import { SearchBar } from '../../components/search-bar/search-bar';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [RouterLink, MatIcon, MatIconButton],
+  imports: [RouterLink, MatIcon, MatIconButton, SearchBar],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.scss',
 })

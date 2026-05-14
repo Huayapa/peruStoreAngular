cc76448b612b39f4318fd591e5d4721472bc07fb
josue Huayapa <huayapajosue0@gmail.com>
2026-05-13 19:17:40 -0500

    feat: add cart detail and checkout pages with basic structure

---
+++ src/app/features/cart/pages/cart-detail/cart-detail.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cart-detail',
+  imports: [],
+  templateUrl: './cart-detail.html',
+  styleUrl: './cart-detail.scss',
+})
+export default class CartDetailPage {}

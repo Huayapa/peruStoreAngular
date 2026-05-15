2224f5ab71a3aa2330bb8f95ae3b388f8cd7b34c
josue Huayapa <huayapajosue0@gmail.com>
2026-05-14 20:17:52 -0500

    feat: enhance cart detail page with product listing and summary, add responsive styles

--- src/app/features/cart/pages/cart-detail/cart-detail.ts
+++ src/app/features/cart/pages/cart-detail/cart-detail.ts
@@ -1,9 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject } from '@angular/core';
+import { MatAnchor, MatMiniFabButton } from '@angular/material/button';
+import { CartProductsService } from '../../../../core/services/cart-products';
+import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
+import { MatIcon } from '@angular/material/icon';
+import { CurrencyPipe } from '@angular/common';
+import { IProduct } from '../../../../shared/interfaces/product.interface';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmDialog } from '../../../../shared/components/confirm-dialog/confirm-dialog';
 
 @Component({
   selector: 'app-cart-detail',
-  imports: [],
+  imports: [MatAnchor, MatIcon, CurrencyPipe, MatMiniFabButton],
   templateUrl: './cart-detail.html',
   styleUrl: './cart-detail.scss',
 })
-export default class CartDetailPage {}
+export default class CartDetailPage {
+  private readonly _cardProducts = inject(CartProductsService);
+  private readonly _dialog = inject(MatDialog);
+  private readonly _destroyRef = inject(DestroyRef);
+
+  cart = toSignal(this._cardProducts.cartproduct$, { initialValue: null });
+  total = toSignal(this._cardProducts.totalPrice$, { initialValue: 0 });
+
+  updateStock(productId: number, e: Event) {
+    const target = e.target as HTMLInputElement;
+    if (Number(target.value) < 1) {
+      target.value = '1';
+      return;
+    }
+    this._cardProducts.updateStock(productId, Number(target.value));
+  }
+
+  deleteProduct(prod: IProduct) {
+    const dialogRef = this._dialog.open(ConfirmDialog, {
+      width: '350px',
+      enterAnimationDuration: '100ms',
+      exitAnimationDuration: '100ms',
+      data: {
+        title: 'Eliminar producto del carrito',
+        message: '¿Seguro que deseas borrarlo?',
+      },
+    });
+    dialogRef
+      .afterClosed()
+      .pipe(takeUntilDestroyed(this._destroyRef))
+      .subscribe((confirmed) => {
+        if (confirmed) {
+          this._cardProducts.removeProductToCart(prod);
+        }
+      });
+  }
+}

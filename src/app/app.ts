d0160e7759069a193642d83956db9ade4db1c7b7
josue Huayapa <huayapajosue0@gmail.com>
2026-05-31 22:09:10 -0500

    feat: add @vercel/analytics dependency and integrate analytics injection in app component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './shared/layout/navbar/navbar';
 import { Footer } from './shared/layout/footer/footer';
+import { inject } from '@vercel/analytics';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, Navbar, Footer],
   templateUrl: './app.html',
 })
-export class App {}
+export class App implements OnInit {
+  ngOnInit(): void {
+    inject();
+  }
+}

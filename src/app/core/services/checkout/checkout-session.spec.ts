514da3674c484825d665383d1b6fb23880e44e7a
josue Huayapa <huayapajosue0@gmail.com>
2026-06-17 20:15:45 -0500

    fix: update test descriptions in CheckoutSessionService tests to English

--- src/app/core/services/checkout/checkout-session.spec.ts
+++ src/app/core/services/checkout/checkout-session.spec.ts
@@ -12,25 +12,25 @@ describe('Checkout Session', () => {
   });
 
   describe('getToken', () => {
-    it('deberia retornar la session token', () => {
+    it('should return session token', () => {
       service.setToken('token_valid');
       expect(service.getToken()).toEqual('token_valid');
     });
 
-    it('deberia retornar null si no existe la session token', () => {
+    it('should return null if session token not exists', () => {
       expect(service.getToken()).toEqual(null);
     });
   });
 
   describe('setToken', () => {
-    it('deberia agregar la session token correctamente', () => {
+    it('should add the session token succeded', () => {
       service.setToken('token_valid');
       expect(service.getToken()).toEqual('token_valid');
     });
   });
 
   describe('removeToken', () => {
-    it('deberia remover el session token', () => {
+    it('should remove session token', () => {
       service.setToken('token_valid');
       service.removeToken();
       expect(service.getToken()).toEqual(null);

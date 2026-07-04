a02cf40f8fbc1d6015a4481604f1c2547bd7b58c
josue Huayapa <huayapajosue0@gmail.com>
2026-07-03 19:49:33 -0500

    fix(breadcrumb): change async pipe by toSignal for breadcrumbs$

--- src/app/shared/components/breadcrumb/breadcrumb.ts
+++ src/app/shared/components/breadcrumb/breadcrumb.ts
@@ -1,17 +1,20 @@
-import { AsyncPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { MatIconButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
 import { BreadcrumbService } from '../../../core/services/breadcrumb/breadcrumb';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { IBreadcrumb } from '../../../core/interfaces/breadcrumb.interface';
 
 @Component({
   selector: 'app-breadcrumb',
-  imports: [AsyncPipe, RouterLink, MatIcon, MatIconButton],
+  imports: [RouterLink, MatIcon, MatIconButton],
   templateUrl: './breadcrumb.html',
   styleUrl: './breadcrumb.scss',
 })
 export class Breadcrumb {
   private _breadcrumb = inject(BreadcrumbService);
-  breadcrumbs$ = this._breadcrumb.breadcrumbs$;
+  readonly breadcrumbs = toSignal(this._breadcrumb.breadcrumbs$, {
+    initialValue: [] as IBreadcrumb[],
+  });
 }
